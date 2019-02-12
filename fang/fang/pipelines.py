60159fe29dc1b50aa0664dfeee3698ae98d04078
leno <hzj.boxes@gmail.com>
2019-02-12 15:17:08 +0800

    修bug

--- fang/fang/pipelines.py
+++ fang/fang/pipelines.py
@@ -5,6 +5,10 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from scrapy.exporters import JsonLinesItemExporter
+import pymysql
+from twisted.enterprise import adbapi
+#专门做数据库处理的模块
+from pymysql import cursors
 
 class FangPipeline(object):
     def __init__(self):
@@ -20,5 +24,31 @@ class FangPipeline(object):
     def close_spider(self,spider):
         self.newhouse_fp.close()
         self.esfhouse_fp.close()
+
+
+# class FangDBPipeline(object):
+#     def __init__(self):
+#         dbparams={'host':'127.0.0.1',
+#                   'port':3306,
+#                   'user': 'root',
+#                   'password': 'root',
+#                   'charset': 'utf8',
+#                   'database': 'newhouse',
+#                   'cursorclass': cursors.DictCursor
+#         }  # 需要游标类
+#         self.conn = pymysql.connect(**dbparams)
+#         self.cursor = self.conn.cursor()
+#         self._sql = None
+#     def process_item(self,item,spider):
+#         self.cursor.execute(self.sql,(item['']))
+#         return item
+#
+#     @property
+#     def sql(self):
+#         if not self._sql:
+
+
+
+
 
 
--- fang/fang/spiders/sfw.py
+++ fang/fang/spiders/sfw.py
@@ -10,23 +10,23 @@ class SfwSpider(scrapy.Spider):
 
     def parse(self, response):
         trs =response.xpath("//div[@class='outCont']//tr")
-        provice =None
+        province =None
         for tr in trs:
             tds =tr.xpath(".//td[not(@class='font01')]")
-            provice_td=tds[0]
-            provice_text =provice_td.xpath(".//text()").get()
-            provice_text =re.sub(r"\s","",provice_text)
-            if provice_text:
-                provice=provice_text
+            province_td=tds[0]
+            province_text =province_td.xpath(".//text()").get()
+            province_text =re.sub(r"\s","",province_text)
+            if province_text:
+                province=province_text
             #不爬取海外
-            if provice =='其它':
+            if province =='其它':
                 continue
             city_td = tds[1]
             city_links =city_td.xpath(".//a")
             for city_link in city_links:
                 city_name = city_link.xpath(".//text()").get()
                 city_url = city_link.xpath(".//@href").get()
-                # print("省份",provice)
+                # print("省份",province)
                 # print('城市',city_name)
                 # print('城市url',city_url)
                 url_module =city_url.split(".")
@@ -38,28 +38,33 @@ class SfwSpider(scrapy.Spider):
                     esf_url="http://esf.fang.com/?ctm=1.bj.xf_search.head.105"
                 else:
                     #新房url
-                    newhouse_url =scheme+'.'+"newhouse."+fang+"."+com+"house/s/"
+                    if "/" in com:
+                        newhouse_url =scheme+'.'+"newhouse."+fang+"."+com+"house/s/"
+                    else:
+                        newhouse_url = scheme + '.' + "newhouse." + fang + "." + com + "/house/s/"
                     #旧房url
                     esf_url =scheme+'.'+"esf."+fang+"."+com
-                yield scrapy.Request(url=newhouse_url,callback=self.parse_newhouse,meta={"info":(provice,city_name)})
-                yield scrapy.Request(url=esf_url, callback=self.parse_esf, meta={"info": (provice, city_name)})
+                yield scrapy.Request(url=newhouse_url,callback=self.parse_newhouse,meta={"info":(province,city_name)})
+                yield scrapy.Request(url=esf_url, callback=self.parse_esf, meta={"info": (province, city_name)})
     def parse_newhouse(self,response):
         province,city =response.meta.get('info')
         #获取yield中的元组
         lis = response.xpath("//div[contains(@class,'nl_con clearfix')]/ul/li[not(@id)]")
         for li in lis:
-            name = "".join(li.xpath(".//div[@class='nlcd_name']/a/text()").get())
+            name = "".join(li.xpath(".//div[contains(@class,'nlcd_name')]/a/text()").getall())
+            name = re.sub(r"\s","",name)
             # if name!=None:
             #     name=name.strip()
             #     print(name)
             house_type_list = li.xpath(".//div[contains(@class,'house_type')]/a/text()").getall()
             house_type_list=list(map(lambda x:re.sub(r"\s","",x),house_type_list))
-            rooms = list(filter(lambda x:x.endswith("居"),house_type_list))
+            rooms_list = list(filter(lambda x:x.endswith("居"),house_type_list))
+            rooms = "".join(rooms_list)
             #print(rooms)
             area="".join(li.xpath(".//div[contains(@class,'house_type')]/text()").getall())
             area = re.sub(r"\s|－|/","",area)
             #print(area)
-            address = li.xpath(".//div[@class = 'address']/a/@title").get()
+            address = "".join(li.xpath(".//div[@class = 'address']/a/@title").getall())
             #print(address)
             district_text = "".join(li.xpath(".//div[@class ='address']/a//text()").getall())
             try:
@@ -73,7 +78,11 @@ class SfwSpider(scrapy.Spider):
             price = "".join(li.xpath(".//div[contains(@class,'nhouse_price')]//text()").getall())
             price = re.sub(r"\s|广告","",price)
             #print(price)
-            origin_url = "http:"+li.xpath(".//div[@class='nlcd_name']/a/@href").get()
+            origin_url_p = "".join(li.xpath(".//div[@class='nlcd_name']/a/@href").getall())
+            origin_url = response.urljoin(origin_url_p)
+
+            # detail_url = "".join(dl.xpath(".//h4[@class='clearfix']/a/@href").getall())
+            # item['origin_url'] = response.urljoin(detail_url)
             #print(origin_url)
 
             item  =NewHouseItem(province=province,city=city,name=name,rooms=rooms,address=address,area=area,district=district,price=price,sale=sale,origin_url=origin_url)
@@ -81,7 +90,7 @@ class SfwSpider(scrapy.Spider):
 
         next_url = response.xpath("//div[@class='page']/a[@class='next']/@href").get()
         if next_url:
-            yield scrapy.Request(url=response.urljoin(next_url),callback=self.parse_newhouse,meta={"info":(provice,city)})
+            yield scrapy.Request(url=response.urljoin(next_url),callback=self.parse_newhouse,meta={"info":(province,city)})
 
 
 
@@ -93,6 +102,7 @@ class SfwSpider(scrapy.Spider):
         for dl in dls:
             item = ESFHouseItem(province=province,city=city)
             name = ''.join(dl.xpath(".//dd//p[@class='add_shop']/a/@title").getall())
+            name = re.sub(r"\s", "", name)
             item['name']=name
             infos = dl.xpath(".//dd//p[@class='tel_shop']//text()").getall()
             infos = list(map(lambda x:re.sub(r"\s|\|",'',x),infos))
@@ -108,7 +118,7 @@ class SfwSpider(scrapy.Spider):
                     item['toward']=info
                 elif '㎡' in info:
                     item['area']=info
-            address = dl.xpath(".//dd//p[@class='add_shop']//span//text()").getall()
+            address = "".join(dl.xpath(".//dd//p[@class='add_shop']//span//text()").getall())
             item['address']=address
             price = "".join(dl.xpath(".//dd[@class='price_right']//span[@class='red']//text()").getall())
             item['price'] = price
